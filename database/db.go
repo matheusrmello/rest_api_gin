5f3f2730c022c0799f5e751bdf6b6a8b58b21ca1
Matheus Mello <50120961+matheusrmello@users.noreply.github.com>
2023-11-17 17:11:24 -0300

    Change docker compose

--- database/db.go
+++ database/db.go
@@ -14,7 +14,7 @@ var (
 )
 
 func ConnectionDB() {
-	stringConexao := "host=localhost user=root password=root dbname=postgres port=5431 sslmode=disable"
+	stringConexao := "host=localhost user=root password=root dbname=postgres port=5432 sslmode=disable"
 	DB, err = gorm.Open(postgres.Open(stringConexao))
 	if err != nil {
 		log.Panic("Erro ao se conectar com o banco de dados")
