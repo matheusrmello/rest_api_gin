21986af340436358387ff0a630ee7ed06addbf85
matheusrmello <matheus.rm.09@hotmail.com>
2023-11-30 10:28:13 -0300

    Updating db.go

--- database/db.go
+++ database/db.go
@@ -2,6 +2,7 @@ package database
 
 import (
 	"log"
+	"os"
 
 	"github.com/matheusrmello/api-go-gin/models"
 	"gorm.io/driver/postgres"
@@ -14,7 +15,7 @@ var (
 )
 
 func ConnectionDB() {
-	stringConexao := "host=localhost user=root password=root dbname=postgres port=5432 sslmode=disable"
+	stringConexao := "host="+os.Getenv("HOST")+" user="+os.Getenv("USER")+" password="+os.Getenv("PASSWORD")+" dbname="+os.Getenv("DBNAME")+" port="+os.Getenv("DBPORT")+" sslmode=disable"
 	DB, err = gorm.Open(postgres.Open(stringConexao))
 	if err != nil {
 		log.Panic("Erro ao se conectar com o banco de dados")
